6da275fc1f47fc0f15a5282e4076cc1b4b2afc17
SteveLiu <steve.liu.ping@gmail.com>
2025-11-12 02:46:56 +0800

    [perf]optimize merge operation in bpe, having 10x improvement

--- assignment1-basics/scripts/train_tokenizer.sh
+++ assignment1-basics/scripts/train_tokenizer.sh
@@ -3,14 +3,14 @@
 # using whole train set
 # uv run train/train_tokenizer.py \
 #     --output_dir 'model/my_tokenizer' \
-#     --vocab_size 20000 \
+#     --vocab_size 10000 \
 #     --data_path 'data/TinyStoriesV2-GPT4-train.txt' \
 #     > train_logs/train_tokenizer.log
 
 # using valid set to train, for validate the train process
 uv run train/train_tokenizer.py \
     --output_dir 'model/my_tokenizer' \
-    --vocab_size 10000 \
+    --vocab_size 1000 \
     --data_path 'data/TinyStoriesV2-GPT4-valid.txt' \
     > train_logs/train_tokenizer.log
 
