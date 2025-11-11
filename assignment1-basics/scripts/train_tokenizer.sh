c7cc3afc2b48bd59bb2a67ad4435ab9155ea658b
SteveLiu <steve.liu.ping@gmail.com>
2025-11-11 23:30:30 +0800

    [feat]add train script for train_bpe

---
+++ assignment1-basics/scripts/train_tokenizer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# using whole train set
+# uv run train/train_tokenizer.py \
+#     --output_dir 'model/my_tokenizer' \
+#     --vocab_size 20000 \
+#     --data_path 'data/TinyStoriesV2-GPT4-train.txt' \
+#     > train_logs/train_tokenizer.log
+
+# using valid set to train, for validate the train process
+uv run train/train_tokenizer.py \
+    --output_dir 'model/my_tokenizer' \
+    --vocab_size 10000 \
+    --data_path 'data/TinyStoriesV2-GPT4-valid.txt' \
+    > train_logs/train_tokenizer.log
+
+# using the minimum test set
+# uv run train/train_tokenizer.py \
+#     --output_dir 'model/my_tokenizer' \
+#     --vocab_size 500 \
+#     --data_path 'tests/fixtures/corpus.en' \
+#     > train_logs/train_tokenizer.log
