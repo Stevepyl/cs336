a74124b535b951b4734d552a38cc0ba799aec165
SteveLiu <71480459+Stevepyl@users.noreply.github.com>
2026-03-06 15:00:11 +0000

    [fix]update tokenizer training script

--- assignment1-basics/scripts/train_tokenizer.sh
+++ assignment1-basics/scripts/train_tokenizer.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 # 如果运行脚本时提供了第 1 个参数，就把它赋给 train_mode；否则使用默认值 "full"。
-train_mode="${1:-full}"
+train_mode="${1:-tinystories_full}"
 comments="${2:-}"
 
 case "$train_mode" in 
     test)
         data_path='tests/fixtures/corpus.en'
         vocab_size=500
+        tokenizer='tinystories'
         ;;
     mini)
         data_path='data/TinyStoriesV2-GPT4-valid.txt'
         vocab_size=1000
+        tokenizer='tinystories'
         ;;
-    full)
+    tinystories_full)
         data_path='data/TinyStoriesV2-GPT4-train.txt'
         vocab_size=32000
+        tokenizer='tinystories'
         ;;
     owt_full)
         data_path='data/owt_valid.txt'
         vocab_size=32000
+        tokenizer='owt'
         ;;
     *)
         echo "Unknown train_mode: $train_mode" >&2
@@ -28,7 +32,7 @@ case "$train_mode" in
 esac    
 
 uv run train/train_tokenizer.py \
-  --output_dir 'model/my_tokenizer' \
+  --output_dir tokenizer/$tokenizer \
   --vocab_size "$vocab_size" \
   --data_path "$data_path" \
   > train_logs/train_tokenizer_${train_mode}_${comments}.log
